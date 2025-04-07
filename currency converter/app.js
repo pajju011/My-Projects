60358d9c0f2b4e090cdddcfead84977784fa93f9
pajju011 <prajwal.r.poojary11@gmail.com>
2025-04-07 22:47:36 +0530

    ##

--- currency converter/app.js
+++ currency converter/app.js
@@ -1,1 +1,7 @@
-const URL= "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+const URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+for (code in countryList) {
+  console.log(code);
+}
