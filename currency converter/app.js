4e11f297779e2199f4d9c39ed7ae7bf7f73bb2f0
pajju011 <prajwal.r.poojary11@gmail.com>
2025-04-09 00:37:27 +0530

    flag change completed

--- currency converter/app.js
+++ currency converter/app.js
@@ -1,13 +1,33 @@
+//api
 const URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json";
 
+// country options list
 const dropdown = document.querySelectorAll(".dropdown select");
 
 for (let select of dropdown) {
-  for (currcode in countryList) {
+  for (let currcode in countryList) {
     let newOption = document.createElement("option");
     newOption.innerText = currcode;
     newOption.value = currcode;
+    if (select.name === "from" && currcode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currcode === "INR") {
+      //dont give extra space in name
+      newOption.selected = "selected";
+    }
     select.append(newOption);
   }
+  select.addEventListener("change", (evt) => {
+    updateflag(evt.target);
+  });
 }
+
+//flag change while option change
+const updateflag = (element) => {
+  let currcode = element.value;
+  let countrycode = countryList[currcode]; //IN,EU...
+  let newsrc = `https://flagsapi.com/${countrycode}/flat/64.png`;
+  let newimg = element.parentElement.querySelector("img");
+  newimg.src = newsrc;
+};
