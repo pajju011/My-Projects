79e4312618f8e8c96cab15ba89b1660b7d3f3f55
pajju011 <prajwal.r.poojary11@gmail.com>
2025-04-09 00:00:11 +0530

    option updated

--- currency converter/app.js
+++ currency converter/app.js
@@ -2,7 +2,12 @@ const URL =
   "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/eur.json";
 
 const dropdown = document.querySelectorAll(".dropdown select");
-for (code in countryList) {
-  console.log(code,countryList[code]);
+
+for (let select of dropdown) {
+  for (currcode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currcode;
+    newOption.value = currcode;
+    select.append(newOption);
+  }
 }
-  
